66f7ed0ad2afa616f15d20e5a1c28e30bafd5de0
Jerry <mikeynjerry@gmail.com>
2018-10-08 01:59:27 -0400

    GitHub Issue Data
    Added all the GitHub issues from our six frameworks.

---
+++ data/issues/clean_issues.py
@@ -0,0 +1,14 @@
+import json
+
+repos = ['react', 'angular', 'vue', 'backbone', 'ember.js', 'jquery']
+fields = ['url', 'comments_url', 'title', 'created_at', 'updated_at', 'body']
+
+for repo in repos:
+  with open('%s_issues_dirty.json' % repo, 'r') as dirty:
+    issues = json.loads(dirty.read())
+    for issue in issues:
+      for field in list(issue.keys()):
+        if field not in fields:
+          del issue[field]
+  with open('%s_issues_clean.json' % repo, 'w+') as clean:
+    json.dump(issues, clean)
