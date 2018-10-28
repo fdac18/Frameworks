fadb258503556b410cd6a6dc5e8cb22b456f50d5
Jerry <mikeynjerry@gmail.com>
2018-10-28 03:50:16 -0400

    Data By Month Then Framework

--- data/github/sort_by_month.py
+++ data/github/sort_by_month.py
@@ -3,9 +3,8 @@ import json, os
 
 repos = ['react', 'angular', 'vue', 'backbone', 'ember.js', 'jquery']
 
-data = {}
-
 for repo in repos:
+  data = {}
   with open('%s_issues_clean.json' % repo, 'r') as clean:
     issues = json.loads(clean.read())
     for issue in issues:
@@ -22,11 +21,11 @@ for repo in repos:
       except KeyError:
         data[comment['d']] = []
 
-for key in data.keys():
-  time = key.split('/')
-  path = '../data_by_month/%s-%s' % (time[0], time[1])
-  if os.path.isdir(path) is not True:
-    os.mkdir(path)
-  with open(path + '/data.json', "w+") as f:
-    json.dump(data[key], f)
+  for key in data.keys():
+    time = key.split('/')
+    path = '../data_by_month/%s-%s' % (time[0], time[1])
+    if os.path.isdir(path) is not True:
+      os.mkdir(path)
+    with open(path + '/%s.json' % repo, "w+") as f:
+      json.dump(data[key], f)
 
