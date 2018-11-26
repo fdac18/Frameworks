7174fb85ce3f42d5b0ced44d065dcd237a24cfdb
Jerry <mikeynjerry@gmail.com>
2018-11-25 20:43:19 -0500

    Fixed Formula

--- data/downloads/growth.py
+++ data/downloads/growth.py
@@ -18,17 +18,17 @@ def find_growth_between(pkg, old_month, new_month):
     if (old_dl == 0): old_dl = 1
     new_dl = dl_rate[new_month]
     if (new_dl == 0): new_dl = 1
-    percent = (new_dl / old_dl) * 100
+    percent = ((new_dl / old_dl) - 1) * 100
     change = 'grew'
     if (percent < 0):
       change = 'declined'
     percent_str = float("{0:.2f}".format(percent))
 
-    return percent, "{}'s download rate {} by {}% between {} and {}".format(pkg, change, percent_str, old_month, new_month)
+    return percent, "{}'s download rate {} by {}% between {} and {}".format(pkg, change, percent_str, old_month, new_month), dl_rate
 
 if __name__ == '__main__':
   pkg = sys.argv[1]
   old_month = sys.argv[2]
   new_month = sys.argv[3]
-  percent, growth_str = find_growth_between(pkg, old_month, new_month)
+  percent, growth_str, dl_rates_by_month = find_growth_between(pkg, old_month, new_month)
   print(growth_str)
