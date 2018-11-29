8cdaf2392c113981d8244c493fe56e7c7a08623a
Preston Provins <pprovins@vols.utk.edu>
2018-11-29 13:55:45 -0500

    Update data/overflow/packageData.py

--- data/overflow/packageData.py
+++ data/overflow/packageData.py
@@ -1,1 +1,34 @@
-import
+import datetime
+import pprint
+
+from pymongo import MongoClient
+
+
+client = MongoClient('da1')
+sodata = client.fdac18stackoverflow
+postcol = sodata.posts
+
+pipeline = [
+  {
+    "$match": {
+      "Tags": "jquery",
+      "CreationDate": {"$gte": datetime.datetime(2015, 1, 1, 0, 0, 0)}
+    }
+  },
+  {
+    "$group": {
+      "_id": {
+        "$concat": [
+          {"$substr": [{"$year": "$CreationDate"}, 0, 4]},
+          "/",
+          {"$substr": [{"$month": "$CreationDate"}, 0, 4]},
+          "/",
+          {"$substr": [{"$dayOfMonth": "$CreationDate"}, 0, 4]}
+        ]
+      },
+      "c": {"$sum": 1}
+    }
+  }
+]
+
+pprint.pprint(postcol.aggregate(pipeline))
