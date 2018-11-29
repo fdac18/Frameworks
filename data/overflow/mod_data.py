ce4a333cb170648dfff627f2cede1cb9bfa31d7c
Preston Provins <pprovins@vols.utk.edu>
2018-11-29 16:24:30 -0500

    Update data/overflow/mod_data.py

--- data/overflow/mod_data.py
+++ data/overflow/mod_data.py
@@ -3,6 +3,9 @@ from os import walk
 f = []
 for (dirpath, dirnames, filenames) in walk("."):
     f.extend(filenames)
+    for afile in filenames:
+        if '.json' in afile:
+            print(afile)
     break
 
 print(f)
